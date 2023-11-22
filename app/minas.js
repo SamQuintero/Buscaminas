f8653883ab8fc4db9783bc5a7108c61f91166adb
Luis <lraulam.03@gmail.com>
2023-11-21 18:04:41 -0600

    Loading Board and timer

--- app/minas.js
+++ app/minas.js
@@ -1,3 +1,22 @@
+const timer = document.getElementById("timer");
+let seconds = 0;
+let minutes = 0;
+
+let game_on = true;
+
+setInterval(() => {
+    if (!game_on) return;
+    seconds++;
+    if (seconds == 60) {
+        minutes++;
+        seconds = 0;
+    }
+    timer.innerHTML = 
+        (minutes < 10 ? "0" + minutes: minutes) + ":" + (seconds < 10 ? "0" + seconds: seconds);
+    sessionStorage.setItem("timer", seconds + (minutes * 60));
+}, 1000);
+
+
 function loadBoard() {
     let difficulty_options = document.getElementsByName("difficulty_level");
     let difficulty;
