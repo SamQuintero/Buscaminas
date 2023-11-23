a77bb85dfdcb5fc5ec41f5c2508a8b1b048c9a56
Luis <lraulam.03@gmail.com>
2023-11-22 23:36:00 -0600

    BackendCalls

--- router.js
+++ router.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const path = require ('path');
 const users = require("./routes/users");
+const scores = require("./routes/scores");
+
 
 router.use("/users", users);
+router.use("/scores", scores);
 
 module.exports = router;
